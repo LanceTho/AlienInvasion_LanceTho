7e6d21490a0507e3df27d4e40be9a44aea961aa2
Lance Thongsavanh <lancer8106@gmail.com>
2026-04-15 18:16:38 -0400

    Created the alien, displayed it on screen in the right direction, does not move

---
+++ alien.py
@@ -0,0 +1,46 @@
+"""
+alien.py
+Lance Thongsavanh
+This file has the Alien class
+4/15/2026
+"""
+
+import pygame
+from pygame.sprite import Sprite
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from alien_invasion import AlienInvasion
+
+class Alien(Sprite):
+
+    def __init__(self, game: "AlienInvasion", x: float, y: float) -> None:
+        """Initializes variables based on the game
+
+        Args:
+            game (AlienInvasion): the current game
+            x (float): _description_
+            y (float): _description_
+        """
+        super().__init__()
+        self.screen = game.screen
+        self.boundaries = self.screen.get_rect()
+        self.settings = game.settings
+
+        self.image = pygame.image.load(self.settings.alien_file)
+        self.image = pygame.transform.scale(self.image, (self.settings.alien_width, self.settings.alien_height))
+        self.image = pygame.transform.rotate(self.image, 270)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = int(x)
+        self.rect.y = int(y)
+
+    def update(self) -> None:
+        """Moves the alien
+        """
+        pass
+
+    def draw_alien(self) -> None:
+        """Displays the alien
+        """
+        self.screen.blit(self.image, self.rect)
