50bc5260a9543b7f953e62a8eb63a25d1ca505cf
Lance Thongsavanh <lancer8106@gmail.com>
2026-04-22 14:34:20 -0400

    Fleshed out Game Stats to update the high score and saves it and reads from a file

--- game_stats.py
+++ game_stats.py
@@ -5,6 +5,7 @@ This file holds the GameStats class
 4/15/2026
 """
 
+import json
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -23,7 +24,28 @@ class GameStats():
         self.game = game
         self.settings = game.settings
         self.max_score = 0
+        self.init_saved_scores()
         self.reset_stats()
+
+    def init_saved_scores(self):
+        self.path = self.settings.scores_file
+        if self.path.exists() and self.path.stat.__sizeof__() > 80:
+            contents = self.path.read_text()
+            scores = json.loads(contents)
+            self.high_score = scores.get("high_score", 0)
+        else:
+            self.high_score = 0
+            self.save_scores()
+
+    def save_scores(self) -> None:
+        scores = {
+            "high_score": self.high_score
+        }
+        contents = json.dumps(scores, indent=4)
+        try:
+            self.path.write_text(contents)
+        except FileNotFoundError as e:
+            print(f"File Not Found: {e}")
 
     def reset_stats(self) -> None:
         self.ships_left = self.settings.starting_ship_amount
@@ -31,25 +53,21 @@ class GameStats():
         self.level = 1
 
     def update(self, colliisions) -> None:
-        # update score
         self._update_score(colliisions)
-
-        # update max_score
         self._update_max_score()
-    
-        # update high_score
+        self._update_high_score()
 
-
-    def _update_score(self, colliisions):
+    def _update_score(self, colliisions) -> None:
         for alien in colliisions.values():
             self.score += self.settings.alien_points
-        #print(f"Basic: {self.score}")
     
-    def _update_max_score(self):
+    def _update_max_score(self) -> None:
         if self.score > self.max_score:
             self.max_score = self.score
-        #print(f"Max: {self.max_score}")
+
+    def _update_high_score(self) -> None:
+        if self.score > self.high_score:
+            self.high_score = self.score
 
     def update_level(self) -> None:
         self.level += 1
-        #print(self.level)
